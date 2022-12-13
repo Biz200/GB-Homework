d4920d682e3791a8d976e556dae16ff90688f553
Bogdan <bizik200@yandex.ru>
2022-12-13 21:30:35 +0700

    v. 1.2

--- Sem2/Task1/Program.cs
+++ Sem2/Task1/Program.cs
@@ -1,12 +1,15 @@
 ﻿Console.Clear();
+
 int Second(int numbers)
 {
     Console.Write("Введите трехзначное число: ");
     numbers = int.Parse(Console.ReadLine());
     return numbers;
 }
+
 int numbers = 0;
 int result = Second(numbers);
 result = result / 10 % 10;
 
-Console.WriteLine(result);
+Console.WriteLine($"Вторая цифра: {result}");
+
