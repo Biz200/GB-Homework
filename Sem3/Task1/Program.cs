bacd017324ddd17901ee9cac0154bf97a4f3080e
Bogdan <bizik200@yandex.ru>
2022-12-13 22:26:26 +0700

    Complete sem3 tasks

---
+++ Sem3/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int pal = number.Length;
+
+if (pal == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"{number} - не является пятизначным");
+}
