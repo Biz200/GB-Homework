d6dee598f0299951493675b5fe0cb84ef8541ea3
Bogdan <bizik200@yandex.ru>
2022-12-13 21:19:29 +0700

    Complete sem2 tasks

---
+++ Sem2/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int ThirdNumber(int numbers)
+{
+    Console.Write("Введите трехзначное число: ");
+    numbers = int.Parse(Console.ReadLine());
+    return numbers;
+}
+
+void Third (int numbers)
+{
+    if(numbers >= 100 || numbers <= -100)
+    {
+        while (numbers > 1000|| numbers < -1000 ){numbers = numbers/10;}
+        int thirdnumb = numbers % 10;
+        if(thirdnumb < 0 ){ thirdnumb =  thirdnumb * -1; }
+        Console.WriteLine($"Третья цифра: {thirdnumb}");
+    }
+    else{Console.WriteLine("Третьей цифры нет");}
+}
+
+int numbers = 0;
+int number = ThirdNumber(numbers);
+Third(number);
---
+++ Sem2/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int Numbers (int number)
+{
+    Console.WriteLine("Введите цифру дня недели");
+    number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void Day(int number)
+{
+    if(number == 6 || number == 7){Console.WriteLine("Это выходной");}
+    else{Console.WriteLine("Это не выходной");}
+}
+
+int number = 0;
+int DayNumber = Numbers(number);
+Day(DayNumber);
